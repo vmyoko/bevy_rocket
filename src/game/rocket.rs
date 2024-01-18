76234c4ea169f837d7c290d544caf6e6e6909861
Vinícius Wataru Moreira Yokoyama <vini.moreirayokoyama@gmail.com>
2024-01-17 22:15:48 -0300

    Showing the Rocket

--- src/game/rocket.rs
+++ src/game/rocket.rs
@@ -4,10 +4,19 @@ pub struct RocketPlugin;
 
 impl Plugin for RocketPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, hello);
+        app.add_systems(Startup, setup);
     }
 }
 
-fn hello() {
-    println!("Hello");
+#[derive(Component, Default)]
+pub struct Rocket {
+
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((Rocket::default(), SpriteBundle {
+        texture: asset_server.load("cohete_off.png"),
+        transform: Transform::from_xyz(0., 0., 3.),
+        ..Default::default()
+    }));
 }
