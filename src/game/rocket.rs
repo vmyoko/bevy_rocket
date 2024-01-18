c900e6a2fb6bb7841c1ed9d5b7a87b5b89ed3f89
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-01-18 20:48:06 -0300

    Reading boost input
    Co-Authored-by: vmyoko <vini.moreirayokoyama@gmail.com>

--- src/game/rocket.rs
+++ src/game/rocket.rs
@@ -6,20 +6,46 @@ pub struct RocketPlugin;
 
 impl Plugin for RocketPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, boost.run_if(has_boost_input));
     }
 }
-
-#[derive(Component, Default)]
-pub struct Rocket {}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Rocket::default(),
         SpriteBundle {
             texture: asset_server.load("cohete_off.png"),
-            transform: Transform::from_xyz(0., 0., 3.).with_scale(Vec3::new(ROCKET_SCALE, ROCKET_SCALE, ROCKET_SCALE)),
+            transform: Transform::from_xyz(0., 0., 3.).with_scale(Vec3::new(
+                ROCKET_SCALE,
+                ROCKET_SCALE,
+                ROCKET_SCALE,
+            )),
             ..Default::default()
         },
     ));
+}
+
+fn boost(mut rocket: Query<&mut Rocket>) {
+    for mut rocket in &mut rocket {
+        rocket.state = RocktState::Boosting;
+    }
+}
+
+fn has_boost_input(keyboard_input: Res<Input<KeyCode>>) -> bool {
+    keyboard_input.pressed(KeyCode::Space)
+        || keyboard_input.pressed(KeyCode::Up)
+        || keyboard_input.pressed(KeyCode::Down)
+}
+
+#[derive(Default)]
+enum RocktState {
+    #[default]
+    Grounded,
+    Boosting,
+}
+
+#[derive(Component, Default)]
+pub struct Rocket {
+    state: RocktState,
 }
